034d4f33ffca17a7aadf7eb6a7da0e3ab4dca4f5
Anass Benfillous <anasse.benfillous@gmail.com>
2024-09-21 20:53:30 +0100

    Update TableCreation.java

--- src/util/TableCreation.java
+++ src/util/TableCreation.java
@@ -2,32 +2,33 @@ package util;
 
 public class TableCreation {
 	public TableCreation() {
-		// Create global sequence for Components and related tables
-		DBRequest.createTable("global_component_id_seq", "", "SEQUENCE");
-
-		// Create Projects table
-		DBRequest.createTable("Projects", "id SERIAL PRIMARY KEY, name VARCHAR(255), startDate DATE, endDate DATE", "");
 
 		// Create Clients table
-		DBRequest.createTable("Clients", "id SERIAL PRIMARY KEY, name VARCHAR(255), contactInfo VARCHAR(255)", "");
+		DBRequest.createTable("clients",
+				"id SERIAL PRIMARY KEY, name VARCHAR(255) UNIQUE, address VARCHAR(255), phone VARCHAR(255), isProfessional BOOLEAN",
+				"");
+
+		// Create Projects table
+		DBRequest.createTable("projects",
+				"id SERIAL PRIMARY KEY, name VARCHAR(255) UNIQUE, profit_margin FLOAT, total_cost FLOAT, surface FLOAT, client_id INT references clients(id), status VARCHAR(50)",
+				"");
 
 		// Create Components table
-		DBRequest.createTable("Components",
-				"id INT DEFAULT nextval('global_component_id_seq') PRIMARY KEY, type VARCHAR(255), description TEXT",
+		DBRequest.createTable("components",
+				"id SERIAL PRIMARY KEY, name VARCHAR(255), unit_cost FLOAT, quantity FLOAT, type VARCHAR(255), tax_rate FLOAT, project_id INT references projects(id)",
 				"");
 
 		// Create Quotes table
-		DBRequest.createTable("Quotes",
-				"id SERIAL PRIMARY KEY, projectId INT, amount DECIMAL(10, 2), FOREIGN KEY (projectId) REFERENCES Projects(id)",
+		DBRequest.createTable("quotes",
+				"id SERIAL PRIMARY KEY, project_id INT, estimated_amount float, issue_date DATE, validity_date DATE, accepted BOOLEAN, FOREIGN KEY (project_id) REFERENCES projects(id)",
 				"");
 
 		// Create Labors table with inherited structure from Components
-		DBRequest.createTable("Labors", "type VARCHAR(255), description TEXT, hoursWorked DECIMAL(10, 2)",
+		DBRequest.createTable("labors", "hourly_rate FLOAT, hours_worked float, worker_productivity FLOAT",
 				"INHERITS (Components)");
 
 		// Create Materials table with inherited structure from Components
-		DBRequest.createTable("Materials", "name VARCHAR(255), quantity INT, cost DECIMAL(10, 2)",
-				"INHERITS (Components)");
+		DBRequest.createTable("materials", "transport_cost FLOAT, quality_coefficient FLOAT", "INHERITS (Components)");
 
 	}
 }
