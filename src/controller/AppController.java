099e6a0a060525a019bb35045ba65e837a79f46c
Anass Benfillous <anasse.benfillous@gmail.com>
2024-09-23 17:20:18 +0100

    Create AppController.java

---
+++ src/controller/AppController.java
@@ -0,0 +1,59 @@
+package controller;
+
+import view.ClientView;
+import view.MainMenu;
+import view.ProjectView;
+
+public class AppController {
+
+	public static Boolean running = true;
+	private int choice = 0;
+
+	public AppController() {
+
+		while (running) {
+
+			switch (choice) {
+
+			case 0:
+				choice = MainMenu.displayMenu();
+				break;
+			case 1:
+				choice = handleSubMenu();
+				break;
+			case 2:
+				choice = ClientView.displayAllClient();
+				break;
+			case 3:
+				running = false;
+				System.out.println("Exiting the application...");
+				break;
+			}
+
+		}
+	}
+
+	public int handleSubMenu() {
+		int input = MainMenu.displayProjectSubMenu();
+
+		switch (input) {
+
+		case 1:
+			ProjectView.displayProjectCreationForm();
+			break;
+		case 2:
+			ProjectView.displayAllProject();
+			break;
+		case 3:
+			ProjectView.deleteProject();
+			break;
+		case 4:
+			ProjectView.displayQuotes();
+			break;
+		case 5:
+			return 0;
+		}
+		return 0;
+	}
+
+}
