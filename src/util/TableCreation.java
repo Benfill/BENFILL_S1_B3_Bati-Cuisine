eec38d0840e96e07b6b48818de876948f841404b
Anass Benfillous <anasse.benfillous@gmail.com>
2024-09-23 17:21:53 +0100

    Update TableCreation.java

--- src/util/TableCreation.java
+++ src/util/TableCreation.java
@@ -10,17 +10,17 @@ public class TableCreation {
 
 		// Create Projects table
 		DBRequest.createTable("projects",
-				"id SERIAL PRIMARY KEY, name VARCHAR(255) UNIQUE, profit_margin FLOAT, total_cost FLOAT, surface FLOAT, client_id INT references clients(id), status VARCHAR(50)",
+				"id SERIAL PRIMARY KEY, name VARCHAR(255) UNIQUE, profit_margin FLOAT, total_cost FLOAT, surface FLOAT, client_id INT references clients(id) ON DELETE CASCADE, status VARCHAR(50)",
 				"");
 
 		// Create Components table
 		DBRequest.createTable("components",
-				"id SERIAL PRIMARY KEY, name VARCHAR(255), unit_cost FLOAT, quantity FLOAT, type VARCHAR(255), tax_rate FLOAT, project_id INT references projects(id)",
+				"id SERIAL PRIMARY KEY, name VARCHAR(255), unit_cost FLOAT, quantity FLOAT, type VARCHAR(255), tax_rate FLOAT, project_id INT references projects(id) ON DELETE CASCADE",
 				"");
 
 		// Create Quotes table
 		DBRequest.createTable("quotes",
-				"id SERIAL PRIMARY KEY, project_id INT, estimated_amount float, issue_date DATE, validity_date DATE, accepted BOOLEAN, FOREIGN KEY (project_id) REFERENCES projects(id)",
+				"id SERIAL PRIMARY KEY, project_id INT, estimated_amount float, issue_date DATE, validity_date DATE, accepted BOOLEAN DEFAULT false, FOREIGN KEY (project_id) REFERENCES projects(id)",
 				"");
 
 		// Create Labors table with inherited structure from Components
