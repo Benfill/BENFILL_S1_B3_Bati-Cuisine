a0ca3f74ec0813e495de10c4f7e7fbee00b986db
Anass Benfillous <anasse.benfillous@gmail.com>
2024-09-24 20:42:42 +0100

    remove unused method

--- src/view/LaborView.java
+++ src/view/LaborView.java
@@ -7,17 +7,6 @@ import model.Labor;
 public class LaborView {
 	private static double laborCost = 0;
 
-	public static void addLabor() {
-		System.out.print("Enter labor type (e.g., Basic Worker, Specialist): ");
-		// Input labor type
-		System.out.print("Enter hourly rate (€/h): ");
-		// Input hourly rate
-		System.out.print("Enter hours worked: ");
-		// Input hours worked
-		System.out.print("Enter productivity factor (1.0 = standard, > 1.0 = high productivity): ");
-		// Input productivity factor
-	}
-
 	public static double displayLabor(HashMap<String, Labor> labors) {
 		System.out.println();
 		System.out.println("2. Labors:");
